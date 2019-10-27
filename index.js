608c8b9cc05b4db7007c8a38cdc1b3f99eadb4ba
nalesevic <nizam.alesevic@gmail.com>
2019-10-27 15:31:44 +0100

    minor changes

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.listen(port, () => {
     console.log('Server listening on port: ' + port);
  });
 
+ app.get('/', (req,res) => {
+     res.send("Hello World");
+ })
+
  app.use('/home', (req, res, next) => {
      console.log('new visit ' + req.ip);
      res.send("WADDUP");
