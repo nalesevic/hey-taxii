111901a67dea944b0de7423236fd025da67bffa9
nalesevic <nizam.alesevic@gmail.com>
2019-10-31 18:07:50 +0100

    basic endpoints

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
 const express = require('express');
+const mongojs = require('mongojs');
+const bodyParser = require('body-parser');
+const db = mongojs('mongodb+srv://nizam:taxi@cluster0-bfjrt.mongodb.net/HeyTaxi?retryWrites=true&w=majority', []);
+
 const app = express();
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log('Server listening on port: ' + port);
- });
 
- app.get('/', (req,res) => {
-     res.send("Hello World");
- })
+app.use(express.static('public'));
+app.use(bodyParser.json());
 
- app.use('/home', (req, res, next) => {
-     console.log('new visit ' + req.ip);
-     res.send("WADDUP");
-     next();
- });
+app.get('/driver', (req, res) => {
+    db.driver.find({}, (error, docs) => res.json(docs));
+})
 
- app.get('/user/:id', (req, res) => {
-     res.send("Your id " + req.params.id);
- })
+app.get('/driver/:id', (req, res) => {
+    var id = req.params.id;
+    db.driver.findOne({ _id: mongojs.ObjectID(id) }, (error, docs) => res.json(docs));
+})
+
+app.post('/driver', (req, res) => {
+    db.driver.insert(req.body, (error, doc) => res.json(doc));
+})
+
+app.put('/driver/:id', (req, res) => {
+    var id = req.params.id;
+    db.driver.findAndModify({
+        query: { _id: mongojs.ObjectID(id) },
+        update: { $set: req.body },
+        new: true
+    }, (error, doc, lastErrorObject) => res.json(doc));
+})
+
+app.delete('/driver/:id', (req, res) => {
+    var id = req.params.id;
+    db.driver.remove({ _id: mongojs.ObjectID(id) }, [true], (error, doc) => res.json(doc));
+})
+
+app.listen(port, () => console.log("Listening on port " + port));
