d024c4e18b218c70d131bb08105cc2104f4fb414
Nizam Alešević <nizam.alesevic@gmail.com>
2019-11-07 18:23:22 +0100

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    MONGODB_URL: 'mongodb+srv://nizam:taxi@cluster0-bfjrt.mongodb.net/HeyTaxi?retryWrites=true&w=majority',
-    CLIENT_ID: '660839840912-1bli3ugrqp7tqjse7d5og4ephc1d0s6s.apps.googleusercontent.com',
-    CLIENT_SECRET: 'sjddFtBWsRvBPUYE2Xu-9Xo6',
-    REDIRECT_URL: 'http://localhost:3000/login',
-    JWT_SECRET: 'oQ5:zVqf3ZAD,Jt'
-}
