27d55ca6c8ff1ee296056d3839542d16b7472daf
nalesevic <nizam.alesevic@gmail.com>
2020-01-16 03:42:14 +0100

    log

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -29,26 +29,35 @@ module.exports = (router, db, mongojs, config, jwt) => {
         db.user.find({ type: 'company' }, (error, docs) => {
             res.send(docs);
         });
+
     });
 
     router.post('/company', (req, res) => {
+        let data = { id: req.body.id, time: Date.now(), action: 'insert company' };
+        db.log.insert(data);
         req.body.type = 'company';
         db.user.insert(req.body, (error, doc) => res.json(doc));
+
     });
 
     router.put('/company/:id', (req, res) => {
         let id = req.params.id;
+        let data = { id: id, time: Date.now(), action: 'update company' };
+        db.log.insert(data);
+
         db.user.findAndModify({
             query: { _id: mongojs.ObjectId(id) },
             update: { $set: req.body },
             new: true
         }, (error, doc) => res.json(doc));
+
     });
 
     router.delete('/company/:id', (req, res) => {
         let id = req.params.id;
         console.log("brisem " + id);
-        
+        let data = { id: id, time: Date.now(), action: 'update company' };
+        db.log.insert(data);
         db.user.remove({ _id: mongojs.ObjectId(id) }, (error, doc) => res.json(doc));
     });
 
--- api/routes/company.js
+++ api/routes/company.js
@@ -6,11 +6,11 @@ module.exports = (router, db, mongojs, config, jwt) => {
         console.log('Company visit from ' + req.ip);
         let authorization = req.get('Authorization');
         console.log("1");
-        if(authorization) {
+        if (authorization) {
             console.log("2");
 
             jwt.verify(authorization, config.JWT_SECRET || process.env.JWT_SECRET, (error, decoded) => {
-                if(error) {
+                if (error) {
                     console.log("3");
 
                     res.status(401).send('Unauthorized access');
@@ -19,7 +19,7 @@ module.exports = (router, db, mongojs, config, jwt) => {
 
                     let user_type = decoded.userType;
                     console.log(user_type);
-                    if(user_type == 'company') {
+                    if (user_type == 'company') {
                         console.log("5");
 
                         companyID = decoded.id;
@@ -38,47 +38,52 @@ module.exports = (router, db, mongojs, config, jwt) => {
 
     router.put('/password', (req, res) => {
         console.log("updating password " + req.body.email + " " + req.body.password);
-         db.user.findAndModify({
+        let data = { id: companyID, time: Date.now(), action: 'change company password' };
+        db.log.insert(data);
+        db.user.findAndModify({
             query: { email: req.body.email },
             update: { $set: { password: req.body.password } },
             new: true
         }, (error, doc, lastErrorObject) => {
-            if(error)
+            if (error)
                 throw error;
         });
     })
 
     router.put('/profile', (req, res) => {
         console.log("updating company profile " + req.body.companyID);
-        
+        let data = { id: companyID, time: Date.now(), action: 'update company profile' };
+        db.log.insert(data);
         db.user.findAndModify({
             query: { email: req.body.email },
-            update: { $set: { 
-                name: req.body.name, 
-                email: req.body.email, 
-                address: req.body.address,
-                phone: req.body.phone,
-            } },
+            update: {
+                $set: {
+                    name: req.body.name,
+                    email: req.body.email,
+                    address: req.body.address,
+                    phone: req.body.phone,
+                }
+            },
             new: true
         }, (error, doc, lastErrorObject) => {
-            if(error)
+            if (error)
                 throw error;
         });
     })
 
     router.get('/profile', (req, res) => {
         console.log("retrieving company profile");
-        db.user.findOne( { _id: mongojs.ObjectID(companyID) }, (error, doc) => {
-            if(error)
+        db.user.findOne({ _id: mongojs.ObjectID(companyID) }, (error, doc) => {
+            if (error)
                 throw error;
-            
+
             res.json(doc);
-        } )
+        })
     })
-    
+
 
     router.get('/drivers', (req, res) => {
-        db.driver.find({companyID: mongojs.ObjectID(companyID)}, (error, docs) => res.status(200).send(docs));
+        db.driver.find({ companyID: mongojs.ObjectID(companyID) }, (error, docs) => res.status(200).send(docs));
     })
 
     router.get('/vehicles', (req, res) => {
@@ -86,14 +91,16 @@ module.exports = (router, db, mongojs, config, jwt) => {
     })
 
     router.post('/vehicles', (req, res) => {
+        let logData = { id: companyID, time: Date.now(), action: 'insert company vehicles' };
+        db.log.insert(logData);
         let data = {
             ...req.body,
             companyID: mongojs.ObjectID(companyID)
         }
         console.log(data);
-                
+
         db.vehicle.insert(data, (error, doc) => {
-            if(error)
+            if (error)
                 throw error;
             res.json(doc);
         });
@@ -101,7 +108,8 @@ module.exports = (router, db, mongojs, config, jwt) => {
 
     router.put('/vehicles/:id', (req, res) => {
         console.log("updating vehicle " + req.body.year);
-        
+        let data = { id: companyID, time: Date.now(), action: 'update company vehicle' };
+        db.log.insert(data);
         var id = req.params.id;
         db.vehicle.findAndModify({
             query: { _id: mongojs.ObjectID(id) },
@@ -111,26 +119,30 @@ module.exports = (router, db, mongojs, config, jwt) => {
     })
 
     router.delete('/vehicles/:id', (req, res) => {
+        let data = { id: companyID, time: Date.now(), action: 'delete company vehicle' };
+        db.log.insert(data);
         var id = req.params.id;
         db.vehicle.remove({ _id: mongojs.ObjectID(id) }, [true], (error, doc) => res.json(doc));
     })
-    
+
     router.get('/driver/:id', (req, res) => {
         var id = req.params.id;
         db.driver.findOne({ _id: mongojs.ObjectID(id) }, (error, docs) => res.json(docs));
     })
-    
+
     router.post('/drivers', (req, res) => {
         let data = {
             ...req.body,
             companyID: mongojs.ObjectID(companyID)
         }
         console.log(data);
-                
+
         db.driver.insert(data, (error, doc) => res.json(doc));
     })
-    
+
     router.put('/drivers/:id', (req, res) => {
+        let data = { id: companyID, time: Date.now(), action: 'update company driver' };
+        db.log.insert(data);
         var id = req.params.id;
         db.driver.findAndModify({
             query: { _id: mongojs.ObjectID(id) },
@@ -138,8 +150,10 @@ module.exports = (router, db, mongojs, config, jwt) => {
             new: true
         }, (error, doc, lastErrorObject) => res.json(doc));
     })
-    
+
     router.delete('/drivers/:id', (req, res) => {
+        let data = { id: companyID, time: Date.now(), action: 'delete company driver' };
+        db.log.insert(data);
         var id = req.params.id;
         db.driver.remove({ _id: mongojs.ObjectID(id) }, [true], (error, doc) => res.json(doc));
     })
